d790a89f5d316a71d1582d74bd8a8118774973a9
ym4199 <ym4199@gmail.com>
2017-05-29 21:01:09 +0900

    post templates

--- django_prac_app/blog/views.py
+++ django_prac_app/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title':'PostList from post_list view',
+        'posts':posts,
+    }
+    return render(request, 'blog/post_list.html',context=context)
+
+
