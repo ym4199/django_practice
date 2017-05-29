e06579dc146f53aa035aca849f2de470752ee656
ym4199 <ym4199@gmail.com>
2017-05-29 20:05:01 +0900

    my site

--- django_prac_app/mysite/urls.py
+++ django_prac_app/mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include,url
 from django.contrib import admin
-
+from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'' ,include('blog.urls')),
