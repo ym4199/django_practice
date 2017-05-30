38953d562901d47490d77a13f9ea2680464c6e44
ym4199 <ym4199@gmail.com>
2017-05-31 00:47:45 +0900

    create base template base.html, post_list make link to post_detail

--- django_prac_app/blog/urls.py
+++ django_prac_app/blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from . import views
+from blog import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^admin/', admin.site.urls),
+    # views.post_detail(pk=<num>)
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail),
 ]
--- django_prac_app/blog/views.py
+++ django_prac_app/blog/views.py
@@ -5,11 +5,20 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter()
+    posts = Post.objects.all()
     context = {
         'title':'PostList from post_list view',
         'posts':posts,
     }
     return render(request, 'blog/post_list.html',context=context)
+
+
+def post_detail(request,pk):
+    print('post_detail pk:',pk)
+    # post 라는 키값으로 pk또는 id 값이 매개변수로 주어진 pk변수와 같은 post객체를 전
+    context={
+        'post': Post.objects.get(pk=pk),
+    }
+    return render(request, 'blog/post_detail.html',context)
 
 
